0d9ba19a5fbdaaf50782626c78e1a770d480ffeb
June YAMAMOTO <june@yambal.net>
2019-12-02 22:44:13 +0900

    Styled-Components And Sample

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+
+const Test = styled.div`
+  color: red;
+`
 
 const App: React.FC = () => {
   return (
     <div className="App">
+      <Test>Hello Styled Components</Test>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
